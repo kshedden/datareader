a92210d57b117e1d523ca0889316da7589edcbef
Kerby Shedden <kshedden@umich.edu>
2018-05-05 22:48:03 -0400

    rename windows binaries with .exe suffix

--- build.sh
+++ build.sh
@@ -2,6 +2,11 @@
 
 for GOOS in darwin linux windows; do
     for GOARCH in 386 amd64; do
-	env GOOS=$GOOS GOARCH=$GOARCH go build -v -o bin/stattocsv-$GOOS-$GOARCH cmd/stattocsv/stattocsv.go
+	if [ "$GOOS" = "windows" ]
+	then
+	    env GOOS=$GOOS GOARCH=$GOARCH go build -v -o bin/stattocsv-$GOOS-${GOARCH}.exe cmd/stattocsv/stattocsv.go
+        else
+	    env GOOS=$GOOS GOARCH=$GOARCH go build -v -o bin/stattocsv-$GOOS-$GOARCH cmd/stattocsv/stattocsv.go
+        fi
     done
 done
